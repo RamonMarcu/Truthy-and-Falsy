d37bea9eaebce84ef8d762e9459bce82573bb512
Ramon Marcu <70721540+RamonMarcu@users.noreply.github.com>
2020-11-27 18:53:40 +0200

    Add files via upload

---
+++ Truthy and Falsy.js
@@ -0,0 +1,40 @@
+let wordCount = 2;
+
+if (wordCount) {
+  console.log("Great! You've started your work!");
+} else {
+  console.log('Better get to work!');
+}
+
+
+let favoritePhrase = 'This string is definitely empty';
+
+if (favoritePhrase) {
+  console.log("josh.");
+} else {
+  console.log('This string is definitely empty.');
+}
+
+/* if run the first example, console displays: Great! You've started your work!
+josh. */
+
+
+let wordCount = 0;
+
+if (wordCount) {
+  console.log("Great! You've started your work!");
+} else {
+  console.log('Better get to work!');
+}
+
+
+let favoritePhrase = "";
+
+if (favoritePhrase) {
+  console.log("josh.");
+} else {
+  console.log('This string is definitely empty.');
+}
+
+/* if run the second example, console displays: Better get to work!
+This string is definitely empty. */
